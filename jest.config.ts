8d9d0a549eaf5e1e1c3b27ed413cb60fdedb5725
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2022-09-24 16:47:09 +0200

    add sass config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
+  dir: '.'
 });
 
 // Add any custom config to be passed to Jest
@@ -16,4 +16,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 
-import '../styles/globals.css';
+import '@/styles/main.scss';
 import type { AppProps } from 'next/app';
 import { Session } from 'next-auth';
 
