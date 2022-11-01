802914a2685bf56518883957b6b9eb976fad5d06
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2022-11-01 09:37:57 +0100

    remove cache

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { prisma } from 'prisma/prisma';
 import MainPage from '@/src/layouts/MainPage';
 import Dashboard from '@/src/layouts/Dashboard';
 import { WorkspaceProvider } from '@/src/context/WorkspacesContext';
-import WorkspaceList from '@/src/components/dashboard/workspacesList/WorkspacesList';
+import WorkspacesList from '@/src/components/dashboard/workspacesList/WorkspacesList';
 
 interface WorkspaceOutput {
   memberIn: Workspace[];
@@ -26,7 +26,7 @@ const Home: NextPage<{ loggedIn: any; workspaces?: WorkspaceOutput }> = ({
           guestIn={workspaces?.guestIn ?? []}
         >
           <Dashboard>
-            <WorkspaceList />
+            <WorkspacesList />
           </Dashboard>
         </WorkspaceProvider>
       ) : (
