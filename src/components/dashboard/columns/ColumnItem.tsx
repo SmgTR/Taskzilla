539361955ff7429e467a42088650b26a47e0cd9c
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2022-12-12 12:31:07 +0100

    add draggable column

--- src/components/dashboard/columns/ColumnItem.tsx
+++ src/components/dashboard/columns/ColumnItem.tsx
@@ -8,7 +8,7 @@ import Portal from '@/src/hoc/Portal';
 import { NextPage } from 'next';
 import { forwardRef } from 'react';
 
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 
 import AddButton from '../../ui/buttons/AddButton';
 import MoreButton from '../../ui/buttons/MoreButton';
@@ -27,69 +27,87 @@ interface Props {
     clientX?: number;
     destination?: string;
   };
+  columnIndex: number;
 }
 
-const ColumnItem: NextPage<Props> = forwardRef(({ column, projectId, placeholderProps }, ref) => {
-  const [popupContext, popupDispatch] = usePopupContext();
+const ColumnItem: NextPage<Props> = forwardRef(
+  ({ column, projectId, placeholderProps, columnIndex }, ref) => {
+    const [popupContext, popupDispatch] = usePopupContext();
 
-  const hideModalHandler = () => {
-    popupDispatch(disablePopup());
-  };
+    const hideModalHandler = () => {
+      popupDispatch(disablePopup());
+    };
 
-  const showTaskPopupHandler = () => {
-    popupDispatch(setActivePopup({ activePopup: 'task', popupId: column.id }));
-    popupDispatch(setPopupParentId(projectId));
-  };
+    const showTaskPopupHandler = () => {
+      popupDispatch(setActivePopup({ activePopup: 'task', popupId: column.id }));
+      popupDispatch(setPopupParentId(projectId));
+    };
 
-  return (
-    <>
-      <li className={styles.columnContainer}>
-        <div className={styles.columnItem}>
-          <h4 className={styles.headline}>{column.name}</h4>
-          <MoreButton btnType="button" title="Manage column" />
-
-          <AddButton
-            btnText="+"
-            btnType="button"
-            title="Add Task"
-            onClickHandler={showTaskPopupHandler}
-          />
-        </div>
-        <Droppable droppableId={column.id ?? 'drop-list'}>
+    return (
+      <>
+        <Draggable draggableId={column.id ?? ''} index={columnIndex}>
           {(provided) => (
-            <div className={styles.taskList} ref={provided.innerRef} {...provided.droppableProps}>
-              <ul className={styles.dndList}>
-                {column.Task && column.Task?.length > 0
-                  ? column.Task?.map((task, index) => {
-                      return <TaskItem key={task.id} task={task} index={index} />;
-                    })
-                  : ''}
-                {provided.placeholder}
-                {placeholderProps.destination === column.id && (
-                  <li
-                    className={styles.placeholder}
-                    style={{
-                      position: 'absolute',
-                      top: placeholderProps.clientY,
-                      left: placeholderProps.clientX,
-                      height: placeholderProps.clientHeight,
-                      width: placeholderProps.clientWidth
-                    }}
+            <li
+              className={styles.columnContainer}
+              {...provided.draggableProps}
+              ref={provided.innerRef}
+            >
+              <div className="columnWrapper">
+                <div className={styles.columnItem} {...provided.dragHandleProps}>
+                  <h4 className={styles.headline}>{column.name}</h4>
+                  <MoreButton btnType="button" title="Manage column" />
+
+                  <AddButton
+                    btnText="+"
+                    btnType="button"
+                    title="Add Task"
+                    onClickHandler={showTaskPopupHandler}
                   />
-                )}
-              </ul>
-            </div>
+                </div>
+                <Droppable droppableId={column.id ?? 'drop-list'} type="task">
+                  {(provided) => (
+                    <div
+                      className={styles.taskList}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <ul className={styles.dndList}>
+                        {column.Task && column.Task?.length > 0
+                          ? column.Task?.map((task, index) => {
+                              return <TaskItem key={task.id} task={task} index={index} />;
+                            })
+                          : ''}
+                        {provided.placeholder}
+                        {placeholderProps.destination === column.id && (
+                          <li
+                            className={styles.placeholder}
+                            style={{
+                              position: 'absolute',
+                              top: placeholderProps.clientY,
+                              left: placeholderProps.clientX,
+                              height: placeholderProps.clientHeight,
+                              width: placeholderProps.clientWidth
+                            }}
+                          />
+                        )}
+                      </ul>
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            </li>
           )}
-        </Droppable>
-      </li>
-      {popupContext.activePopup === 'task' && popupContext.popupId === column.id && (
-        <Portal>
-          <AddTaskPopup hidePopup={hideModalHandler} columnId={column.id!} />
-        </Portal>
-      )}
-    </>
-  );
-});
+        </Draggable>
+
+        {popupContext.activePopup === 'task' && popupContext.popupId === column.id && (
+          <Portal>
+            <AddTaskPopup hidePopup={hideModalHandler} columnId={column.id!} />
+          </Portal>
+        )}
+      </>
+    );
+  }
+);
 
 ColumnItem.displayName = 'ColumnItem';
 
