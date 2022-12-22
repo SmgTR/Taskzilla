87194c442cd072ce067e2aee8a0285476b92506a
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2022-12-22 12:47:24 +0100

    add notifications page

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,52 +1,28 @@
 import { NextPage } from 'next';
-import { getSession, useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import { prisma } from 'prisma/prisma';
 
 import Dashboard from '@/src/layouts/Dashboard';
 
 import NotificationsContainer from '@/src/containers/dashboard/NotificationsContainer';
+import { useEffect } from 'react';
+import { updateNotifications } from '@/src/network/secure/notifications/updateNotification';
 
 interface Props {
   notifications: NotificationData[];
   loggedIn: any;
 }
 
-const Notifications: NextPage<Props> = ({ notifications }) => {
+const Notifications: NextPage<Props> = () => {
+  useEffect(() => {
+    updateNotifications();
+  }, []);
+
   return (
     <Dashboard>
-      <NotificationsContainer notifications={notifications} />
+      <NotificationsContainer />
     </Dashboard>
   );
 };
 
 export default Notifications;
-
-export async function getServerSideProps(ctx: any) {
-  const session = await getSession(ctx);
-
-  if (session && session.id) {
-    const userId = session.id as string;
-
-    const userNotifications = await prisma.notification.findMany({
-      where: {
-        userId: userId
-      },
-      include: {
-        invitation: true
-      }
-    });
-
-    if (userNotifications) {
-      return {
-        props: { loggedIn: session, notifications: JSON.parse(JSON.stringify(userNotifications)) }
-      };
-    }
-  } else {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: true
-      }
-    };
-  }
-}
